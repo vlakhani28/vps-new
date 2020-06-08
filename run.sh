801d4a4c090bedceac1adcf4ba593dcc969a1d12
Chen Liang <44931363+solo-chen@users.noreply.github.com>
2020-06-08 12:34:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+mkdir ~/.vnc
+echo '#!/bin/sh\nxrdb $HOME/.Xresources\nunset SESSION_MANAGER\nunset DBUS_SESSION_BUS_ADDRESS\n[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup\n[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources\nvncconfig -iconic &\nxfce4-session & startxfce4 & \n' > ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
+
 /app/expect_vnc.sh
 
 exec supervisord -c /app/supervisord.conf
