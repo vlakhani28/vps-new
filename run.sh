75e3ef7bc7483f5042d40c25f272a35ad8097ae5
Chen Liang <44931363+solo-chen@users.noreply.github.com>
2020-06-09 13:27:29 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,4 +15,17 @@ EOF
 chmod +x ~/.vnc/xstartup
 vncserver -depth 32 -pixelformat rgb888 -geometry 1440x900 :1
 
+cat << EOF >  /Desktop/Google Chrome.desktop'
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Google Chrome
+Comment=Access the Internet
+Exec=/usr/bin/google-chrome-stable --no-sandbox --disable-dev-shm-usage
+Icon=google-chrome
+Path=
+Terminal=false
+StartupNotify=true
+EOF
+
 exec supervisord -c /app/supervisord.conf
