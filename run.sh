6006d25815634af614c9518f7644a6154b044cff
Chen Liang <44931363+solo-chen@users.noreply.github.com>
2020-06-16 19:20:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -ex
 /app/expect_vnc.sh
-mkdir -p ~/.vnc
+mkdir -p ~/.vnc ~/.config/xfce4
+tar -xvf /app/panel.tar -C ~/.config/xfce4
+rm -rf /usr/share/icons/Adwaita
+cp -r /usr/share/icons/Humanity-Dark /usr/share/icons/Adwaita
 cat << EOF >  ~/.vnc/xstartup
 #!/bin/sh
 xrdb $HOME/.Xresources
-startxfce4 & 
+startxfce4 &
 EOF
 chmod +x ~/.vnc/xstartup
 vncserver -depth 32 -pixelformat rgb888 -geometry 1440x900 :1
 
-mkdir -p /Desktop
-cat << EOF >  /Desktop/Chrome.desktop
+mkdir -p /root/Desktop
+cat << EOF >  /root/Desktop/Chrome.desktop
 [Desktop Entry]
 Version=1.0
 Type=Application
@@ -24,4 +27,5 @@ Terminal=false
 StartupNotify=true
 EOF
 
+chmod +x /root/Desktop/Chrome.desktop
 exec supervisord -c /app/supervisord.conf
