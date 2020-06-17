8f2f6fc892518f73f93015624c594becf4efb803
Chen Liang <44931363+solo-chen@users.noreply.github.com>
2020-06-17 20:48:49 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 set -ex
-/app/expect_vnc.sh
 mkdir -p ~/.vnc ~/.config/xfce4
 tar -xvf /app/panel.tar -C ~/.config/xfce4
 rm -rf /usr/share/icons/Adwaita
 cp -r /usr/share/icons/Humanity-Dark /usr/share/icons/Adwaita
-cat << EOF >  ~/.vnc/xstartup
-#!/bin/sh
-xrdb $HOME/.Xresources
-vncconfig -iconic &
-startxfce4 &
-EOF
-chmod +x ~/.vnc/xstartup
-vncserver -depth 32 -pixelformat rgb888 -geometry 1440x900 -geometry 800x600 -geometry 1024x768 -geometry 1360x620 -geometry 1366x768 -geometry 1920x1080 :1
 
 mkdir -p /Desktop
 cat << EOF >  /Desktop/Chrome.desktop
